a646178f758730f60120ff81bae922b9634daacc
Paulo Roberto <prmorais1302@gmail.com>
2020-11-05 01:35:07 -0300

    Configurando ESLint

--- src/gql-server.ts
+++ src/gql-server.ts
@@ -3,26 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 // Types query/mutation/subscription
 
 const typeDefs = `
 type Query {
-	totalPosts: Int!
+  totalPosts: Int!
 }
 `;
 
 // Resolvers
 const resolvers = {
-	Query: {
-		totalPosts: () => 42
-	}
+  Query: {
+    totalPosts: () => 42,
+  },
 };
 
 // GraphQL Server
 
 const apolloServer = new ApolloServer({
-	typeDefs, resolvers,
+  typeDefs,
+  resolvers,
 });
 
 apolloServer.listen(process.env.PORT, () => console.log('Servidor Apollo rodando na porta:', process.env.PORT));
--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.get('/rest', (req, res) => {
-	res.json({
-		data: 'Endpoint raiz rest'
-	});
+  res.json({
+    data: 'Endpoint raiz rest',
+  });
 });
 
 app.listen(process.env.PORT, () => console.log('Servidor rodando na porta:', process.env.PORT));
